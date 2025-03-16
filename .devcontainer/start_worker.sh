7920ad57ae6fa1b0d88077584f264cd7381434b3
George Perdrizet <george@perdrizet.org>
2025-03-15 21:31:50 -0400

    Update start_worker.sh
    Moved headnode_hostname file to project root directory so we don't trigger a devcontainer re-build every time we attach a headnode Codespace.

--- .devcontainer/start_worker.sh
+++ .devcontainer/start_worker.sh
@@ -8,6 +8,6 @@
 
 mkdir -p ~/.ssh
 git pull
-gh codespace ssh -c `cat .devcontainer/headnode_hostname` --config > ~/.ssh/config
-ssh -vfN -L 7077:localhost:7077 cs.`cat .devcontainer/headnode_hostname`.main
+gh codespace ssh -c `cat .headnode_hostname` --config > ~/.ssh/config
+ssh -vfN -L 7077:localhost:7077 cs.`cat .headnode_hostname`.main
 sudo /opt/spark/sbin/start-worker.sh localhost:7077 --webui-port 8080
